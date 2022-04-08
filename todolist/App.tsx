d51cf9e9953880e09c1dd0f3108b28ab9a9435f8
layanelsaeed <layanelsaeed2010@gmail.com>
2022-04-08 16:01:35 +0200

    Adding Code

---
+++ todolist/App.tsx
@@ -0,0 +1,216 @@
+import { useState, useEffect } from "react";
+import {
+  Button,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import Constants from "expo-constants";
+import * as SQLite from "expo-sqlite";
+
+function openDatabase() {
+  if (Platform.OS === "web") {
+    return {
+      transaction: () => {
+        return {
+          executeSql: () => {},
+        };
+      },
+    };
+  }
+
+  const db = SQLite.openDatabase("db.db");
+  return db;
+}
+
+const db = openDatabase();
+
+function Items({ done: doneHeading, onPressItem ,onPressDelete }) {
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        `select * from items where done = ?;`,
+        [doneHeading ? 1 : 0],
+        (_, { rows: { _array } }) => setItems(_array)
+      );
+    });
+  }, []);
+
+  const heading = doneHeading ? "Things that are done" : "Things that need to be done:";
+
+  if (items === null || items.length === 0) {
+    return null;
+  }
+
+  return (
+    <View style={styles.sectionContainer}>
+      <Text style={styles.sectionHeading}>{heading}</Text>
+      {items.map(({ id, done, value }) => (
+        <TouchableOpacity
+          key={id}
+          
+          style={{
+            backgroundColor: done ? "#1c9963" : "#fff",
+            borderColor: "#000",
+            borderWidth: 1,
+            padding: 8,
+          }}
+        >
+          <Text style={{ color: done ? "#fff" : "#000" , fontWeight: "bold", fontSize:"20"}}>{value}</Text>
+          <Button title={done ? "Remove":"Done"} onPress={() => onPressItem && onPressItem(id)}></Button>
+          {!done ? <Button title={ "Remove"} onPress={() => onPressDelete && onPressDelete(id)}></Button> :null }
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
+}
+
+export default function App() {
+  const [text, setText] = useState(null);
+  const [forceUpdate, forceUpdateId] = useForceUpdate();
+
+  useEffect(() => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "create table if not exists items (id integer primary key not null, done int, value text);"
+      );
+    });
+  }, []);
+
+  const add = (text) => {
+    // is text empty?
+    if (text === null || text === "") {
+      return false;
+    }
+
+    db.transaction(
+      (tx) => {
+        tx.executeSql("insert into items (done, value) values (0, ?)", [text]);
+        tx.executeSql("select * from items", [], (_, { rows }) =>
+          console.log(JSON.stringify(rows))
+        );
+      },
+      null,
+      forceUpdate
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>My TodoList </Text>
+      {Platform.OS === "web" ? (
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <Text style={styles.heading}>
+            Expo SQlite is not supported on web!
+          </Text>
+        </View>
+      ) : (
+        <>
+          <View style={styles.flexRow}>
+            <TextInput
+              onChangeText={(text) => setText(text)}
+              onSubmitEditing={() => {
+                add(text);
+                setText(null);
+              }}
+              placeholder="Things that need to be done"
+              style={styles.input}
+              value={text}
+            />
+          </View>
+          <ScrollView style={styles.listArea}>
+            <Items
+              key={`forceupdate-todo-${forceUpdateId}`}
+              done={false}
+              onPressItem={(id) =>
+                db.transaction(
+                  (tx) => {
+                    tx.executeSql(`update items set done = 1 where id = ?;`, [
+                      id,
+                    ]);
+                  },
+                  null,
+                  forceUpdate,
+
+                )
+              }
+              onPressDelete={(id) =>
+                db.transaction(
+                  (tx) => {
+                    tx.executeSql(`delete from items where id = ?;`, [id]);
+                  },
+                  null,
+                  forceUpdate
+                )
+              }
+            />
+            <Items
+              done
+              key={`forceupdate-done-${forceUpdateId}`}
+              onPressItem={(id) =>
+                db.transaction(
+                  (tx) => {
+                    tx.executeSql(`delete from items where id = ?;`, [id]);
+                  },
+                  null,
+                  forceUpdate
+                )
+              }
+            />
+          </ScrollView>
+        </>
+      )}
+    </View>
+  );
+}
+
+function useForceUpdate() {
+  const [value, setValue] = useState(0);
+  return [() => setValue(value + 1), value];
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "lightblue",
+    flex: 1,
+    paddingTop: Constants.statusBarHeight,
+  },
+  heading: {
+    fontSize: 22,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  flexRow: {
+    flexDirection: "row",
+  },
+  input: {
+    borderColor: "#000",
+    borderRadius: 4,
+    borderWidth: 1,
+    flex: 1,
+    height: 48,
+    margin: 16,
+    padding: 8,
+  },
+  listArea: {
+    backgroundColor: "white",
+    flex: 1,
+    paddingTop: 16,
+  },
+  sectionContainer: {
+    marginBottom: 16,
+    marginHorizontal: 16,
+  },
+  sectionHeading: {
+    fontSize: 18,
+    marginBottom: 8,
+  },
+});
